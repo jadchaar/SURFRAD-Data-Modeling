80c5ab91657da73c6169284dd9f72b2df85f7f60
Jad Chaar <jad.chaar@gmail.com>
2017-01-13 22:33:08 -0500

    Added more try except statements to handle errors

--- file_retrieval.py
+++ file_retrieval.py
@@ -14,7 +14,7 @@ def retrieve_files():
     ftp.login()
     print('FTP LOGIN SUCCESSFUL!')
 
-    print('\n### PLOTTING PROGRAM ###\n')
+    print('\n### DATA TRANSFER PROGRAM ###\n')
     print('Step 1. Input SURFRAD Station Location')
     print('## Location Options ##\n', *stationNameArray,
           sep=' | ', end=' |\n## Location Options ##')
@@ -37,7 +37,6 @@ def retrieve_files():
         ftp.cwd(ftpURL)
     except ftplib.all_errors as e:
         print(e)
-        ftp.quit()
         sys.exit(1)
 
     print('\nStep 2. Enter Year of Data Needed')
@@ -51,7 +50,6 @@ def retrieve_files():
         ftp.cwd(ftpURLWithYear)
     except ftplib.all_errors as e:
         print(e)
-        ftp.quit()
         sys.exit(1)
 
     print('\nStep 3. Enter Data File(s) to Retrieve')
@@ -60,6 +58,9 @@ def retrieve_files():
 
     downloadAllResponse = input(
         '\nDownload all files in this directory or only a single one? (all/single) ')
+
+    # backup original directory before alteration
+    original_directory = os.getcwd()
 
     if downloadAllResponse == 'all' or downloadAllResponse == 'ALL':
         # change default directory of FTP storage
@@ -70,16 +71,21 @@ def retrieve_files():
                 print('Creating folder...')
                 os.makedirs(os.getcwd() + '/ftp_files')
 
-        original_directory = os.getcwd()
         os.chdir(os.getcwd() + '/ftp_files')
 
         print('\nTransferring files to ' + os.getcwd())
         filenames = ftp.nlst()
         for filename in filenames:
             # ftp.retrbinary('RETR ' + filename, open(filename, 'wb').write)
-            file = open(filename, 'wb')
-            ftp.retrbinary('RETR ' + filename, file.write)
-            file.close()
+            # file = open(filename, 'wb')
+            # ftp.retrbinary('RETR ' + filename, file.write)
+            try:
+                file = open(filename, 'wb')
+                ftp.retrbinary('RETR ' + filename, file.write)
+                file.close()
+            except ftplib.all_errors as e:
+                print(e)
+                sys.exit(1)
         print('File transfer complete!')
     elif downloadAllResponse == 'single' or downloadAllResponse == 'SINGLE':
         filename = input('\nEnter a data file to read: ')
@@ -95,15 +101,25 @@ def retrieve_files():
                 print('Creating folder...')
                 os.makedirs(os.getcwd() + '/ftp_files')
 
-        original_directory = os.getcwd()
         os.chdir(os.getcwd() + '/ftp_files')
 
         print('\nTransferring ' + filename + ' to ' + os.getcwd())
         # ftp.retrbinary('RETR ' + filename, open(filename, 'wb').write)
-        file = open(filename, 'wb')
-        ftp.retrbinary('RETR ' + filename, file.write)
-        print('File transfer complete!')
-        file.close()
+        # file = open(filename, 'wb')
+        # ftp.retrbinary('RETR ' + filename, file.write)
+        # print('File transfer complete!')
+        # file.close()
+        try:
+            file = open(filename, 'wb')
+            ftp.retrbinary('RETR ' + filename, file.write)
+            print('File transfer complete!')
+            file.close()
+        except ftplib.all_errors as e:
+            print(e)
+            sys.exit(1)
+    else:
+        print('Invalid option. Exiting...')
+        sys.exit(1)
 
     ftp.quit()
 
